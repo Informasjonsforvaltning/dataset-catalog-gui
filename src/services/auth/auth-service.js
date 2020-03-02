2bfc99fb34498cd800f40c5d55b3508f0de9dcc9
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-03-02 14:52:29 +0100

    fix: set full URI as redirect URI

--- src/services/auth/auth-service.js
+++ src/services/auth/auth-service.js
@@ -4,7 +4,7 @@ import { Auth } from '../../lib/auth/auth';
 export const authService = new Auth({
   oidcIssuer: getConfig().auth.oidcIssuer,
   clientId: getConfig().auth.oidcClientId,
-  redirectUri: location.origin,
+  redirectUri: location.href,
   logoutRedirectUri: getConfig().registrationHost,
   silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`
 });
