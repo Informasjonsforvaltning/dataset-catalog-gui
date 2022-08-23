ad4e187852926bf12f760c834b006f568ae2bab3
Pooria Mehregan <pooriamehergan@gmail.com>
2022-08-23 10:29:04 +0200

    feat: add redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider as ReactReduxProvider} from 'react-redux';
+import store from '../src/store' 
 import App from './components/app/App';
 
 const container = document.getElementById('root')!;
@@ -7,6 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ReactReduxProvider store={store}>
+      <App />
+    </ReactReduxProvider>
   </React.StrictMode>
 );
