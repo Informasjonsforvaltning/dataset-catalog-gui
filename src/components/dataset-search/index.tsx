d3db2fd83095933c2259b2cf84b5a8171dbb40a7
Pooria Mehregan <pooriamehregan@gmail.com>
2022-10-28 20:00:03 +0200

    feat: buttons

--- src/components/dataset-search/index.tsx
+++ src/components/dataset-search/index.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
 import { localization } from '../../utils/language/localization';
 import Icon from '../icon';
+import Button from '../inputs/button';
 import SC from './styled';
 
 const Search: FC = () => {
   return (
     <SC.Search>
       <SC.StyledInputBox label={localization.searchForDataset} />
-      <SC.SearchButton label={localization.btnFilterOnPerson} endIcon={<Icon name='chevronDownStroke' />} />
-      <SC.SearchButton label={localization.btnFilterOnStatus} endIcon={<Icon name='chevronDownStroke' />} />
+      <Button type='filled' name={localization.button.lastModifiedSearch} endIcon={<Icon name='chevronDownStroke' />} />
+      <Button type='filled' name={localization.button.statusSearch} endIcon={<Icon name='chevronDownStroke' />} />
     </SC.Search>
   );
 };
