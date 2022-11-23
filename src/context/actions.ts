8186c9c226633c00956d738f0483b89d3699d334
Pooria Mehregan <pooriamehregan@gmail.com>
2022-11-23 06:05:36 +0100

    feat: sort UI & logic

--- src/context/actions.ts
+++ src/context/actions.ts
@@ -1,4 +1,9 @@
+import { ThemeProfile } from '@fellesdatakatalog/theme';
+import { CellType } from '../components/table/table-header';
 import { Dataset } from '../utils/types';
+import { FILTER_TYPE, SORT_TYPE } from './table-context/reducer';
+import { Props as ColumnProps } from '../components/table/table-row/row-cell';
+import { Props as RowProps } from '../components/table/table-row';
 
 enum STATUS {
   IDLE = 'idle',
@@ -11,21 +16,38 @@ enum ACTION_TYPE {
   IDLE,
   ERROR,
   LOADING,
+  ADD_TABLE_ROWS,
   FINISHED,
-  GET_DATASET,
   ADD_DATASET,
+  GET_DATASET,
+  GET_DATASETS,
+  CHANGE_THEME,
+  SORT_DATASETS,
+  INIT_TABLE,
   REMOVE_DATASET,
   ADD_CATALOG_ID,
+  FILTER_DATASETS,
+  ADD_TABLE_HEADER,
+  SEARCH_IN_DATASETS,
+  UPDATE_TABLE,
 }
 
 type ACTION =
   | { type: ACTION_TYPE.IDLE }
-  | { type: ACTION_TYPE.LOADING }
   | { type: ACTION_TYPE.ERROR }
+  | { type: ACTION_TYPE.LOADING }
+  | { type: ACTION_TYPE.GET_DATASETS }
   | { type: ACTION_TYPE.FINISHED; payload: Dataset[] }
   | { type: ACTION_TYPE.ADD_CATALOG_ID; payload: string }
-  | { type: ACTION_TYPE.GET_DATASET; payload: { datasetId: string } }
+  | { type: ACTION_TYPE.SORT_DATASETS; payload: SORT_TYPE }
+  | { type: ACTION_TYPE.FILTER_DATASETS; payload: FILTER_TYPE }
   | { type: ACTION_TYPE.ADD_DATASET; payload: { dataset: Dataset } }
-  | { type: ACTION_TYPE.REMOVE_DATASET; payload: { catalogId: string } };
+  | { type: ACTION_TYPE.GET_DATASET; payload: { datasetId: string } }
+  | { type: ACTION_TYPE.CHANGE_THEME; payload: { theme: ThemeProfile } }
+  | { type: ACTION_TYPE.REMOVE_DATASET; payload: { catalogId: string } }
+  | { type: ACTION_TYPE.INIT_TABLE; payload: { datasets: Dataset[] } }
+  | { type: ACTION_TYPE.UPDATE_TABLE; payload: { datasets: Dataset[] } }
+  | { type: ACTION_TYPE.ADD_TABLE_HEADER; payload: { headerColumns: CellType[] } }
+  | { type: ACTION_TYPE.ADD_TABLE_ROWS; payload: { rows: RowProps<ColumnProps>[] } };
 
 export { ACTION, ACTION_TYPE, STATUS };
--- src/pages/datasets-page/populated-table.tsx
+++ src/pages/datasets-page/populated-table.tsx
@@ -1,57 +1,101 @@
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { NavigateFunction, useLocation, useNavigate } from 'react-router-dom';
 
 import Icon from '../../components/icon';
 import Button from '../../components/inputs/button';
 import Table from '../../components/table';
-import { useDatasetsContext } from '../../context/datasets-context';
 import getDate from '../../utils/helpers/date-and-time-formatter';
 import getTag from '../../utils/helpers/tag-finder';
+import { CellType } from '../../components/table/table-header';
+import { ACTION, ACTION_TYPE } from '../../context/actions';
+import { useTableContext, useTableDispatch } from '../../context/table-context';
+import { Dataset } from '../../utils/types';
+import { Props as ColumnProps } from '../../components/table/table-row/row-cell';
+import { Props as RowProps } from '../../components/table/table-row';
+import { SORT_BY_TYPE, SORT_TYPE } from '../../context/table-context/reducer';
 
 const PopulatedTable = () => {
-  const datasetsState = useDatasetsContext();
+  const tableContext = useTableContext();
+  const tableDispatch = useTableDispatch();
   const location = useLocation();
   const navigate = useNavigate();
 
-  const handleRowClick = (id: string) => navigate(`${location.pathname}/${id}`);
+  const tableUpdate = () => (
+    tableDispatch({
+      type: ACTION_TYPE.ADD_TABLE_HEADER,
+      payload: { headerColumns: getHeaderColumns(tableContext.sort, tableDispatch) },
+    }),
+    tableDispatch({
+      type: ACTION_TYPE.ADD_TABLE_ROWS,
+      payload: { rows: getRows(tableContext.datasets, location.pathname, navigate) ?? [] },
+    })
+  );
 
-  const colWidths = {
-    col_1: '70%',
-    col_2: '16%',
-    col_3: '14%',
-  };
+  useEffect(tableUpdate, [tableContext.sort, tableContext.filter]);
 
-  const cols = [
+  return <Table rows={tableContext.rows} cols={tableContext.headerColumns} />;
+};
+
+const colWidths = {
+  col_1: '70%',
+  col_2: '16%',
+  col_3: '14%',
+};
+
+const getCorrectIcon = (sort: SORT_TYPE, columnName: SORT_BY_TYPE): JSX.Element => {
+  if (sort.sortBy !== columnName) return <Icon name='listUnsortedStroke' />;
+  return sort.sortOrder === 'ascending' ? <Icon name='listAscendingStroke' /> : <Icon name='listDescendingStroke' />;
+};
+
+const getHeaderColumns = (sort: SORT_TYPE, tableDispatcher: React.Dispatch<ACTION>): CellType[] => {
+  return [
     {
       sortButton: (
-        <Button name='Title' type='transparent' iconPos='right' endIcon={<Icon name='listUnsortedStroke' />} />
+        <Button
+          name='Title'
+          btnType='transparent'
+          iconPos='right'
+          endIcon={getCorrectIcon(sort, 'title')}
+          onClick={() => tableDispatcher({ type: ACTION_TYPE.SORT_DATASETS, payload: { sortBy: 'title' } })}
+        />
       ),
       width: colWidths.col_1,
     },
     {
       sortButton: (
-        <Button name='Sist endret' type='transparent' iconPos='right' endIcon={<Icon name='listUnsortedStroke' />} />
+        <Button
+          name='Sist endret'
+          btnType='transparent'
+          iconPos='right'
+          endIcon={getCorrectIcon(sort, 'last-modified')}
+          onClick={() => tableDispatcher({ type: ACTION_TYPE.SORT_DATASETS, payload: { sortBy: 'last-modified' } })}
+        />
       ),
       width: colWidths.col_2,
     },
     {
       sortButton: (
-        <Button name='Status' type='transparent' iconPos='right' endIcon={<Icon name='listUnsortedStroke' />} />
+        <Button
+          name='Status'
+          btnType='transparent'
+          iconPos='right'
+          endIcon={getCorrectIcon(sort, 'status')}
+          onClick={() => tableDispatcher({ type: ACTION_TYPE.SORT_DATASETS, payload: { sortBy: 'status' } })}
+        />
       ),
       width: colWidths.col_3,
     },
   ];
-
-  const rows: any[] = datasetsState.datasets.map(dataset => ({
-    row: [
-      { text: dataset.title?.nb, width: colWidths.col_1 },
-      { text: getDate(dataset._lastModified), width: colWidths.col_2 },
-      { tag: getTag(dataset.registrationStatus), width: colWidths.col_3 },
-    ],
-    onRowClick: () => handleRowClick(dataset.id),
-  }));
-
-  return <Table rows={rows} cols={cols} />;
 };
+
+const getRows = (datasets: Dataset[], location: string, navigate?: NavigateFunction): RowProps<ColumnProps>[] =>
+  datasets.map(dataset => ({
+    row: [
+      { text: dataset.title?.nb ?? 'Empty', width: colWidths.col_1 },
+      { text: getDate(dataset?._lastModified), width: colWidths.col_2 },
+      { tag: getTag(dataset?.registrationStatus), width: colWidths.col_3 },
+    ],
+    onRowClick: navigate ? () => navigate(`${location}/${dataset.id}`) : () => {},
+  }));
 
 export default PopulatedTable;
