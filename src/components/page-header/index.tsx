02ede7842394c1d7052928f51be2ca45af6d45b8
Hege Aalvik <hegeaal@gmail.com>
2023-11-01 13:37:13 +0100

    feat: update to header with link to catalog-admin

--- src/components/page-header/index.tsx
+++ src/components/page-header/index.tsx
@@ -7,12 +7,29 @@ import { authService } from '../../utils/authentication/auth-service';
 import { withAuth } from '../../utils/authentication/auth-provider';
 import { localization } from '../../utils/language/localization';
 import { useGlobalContext } from '../../context/global-context';
+import { useLocation } from 'react-router-dom';
 
-const { FDK_BASE_URI, ADMIN_GUI_BASE_URI, FDK_COMMUNITY_BASE_URI, FDK_REGISTRATION_BASE_URI } = env;
+const { FDK_BASE_URI, ADMIN_GUI_BASE_URI, FDK_COMMUNITY_BASE_URI, FDK_REGISTRATION_BASE_URI, CATALOG_ADMIN_BASE_URI } =
+  env;
 
 const Header = () => {
   const globalContext = useGlobalContext();
   const themeProfile = globalContext.theme;
+
+  const showManageConceptCatalogsUrl = () => {
+    const resourceRoles = authService.getResourceRoles();
+    const location = useLocation();
+    const pathParts = location.pathname.split('/');
+    const currentCatalogId = pathParts[2];
+
+    for (const role of resourceRoles) {
+      const roleCatalogId = role?.resourceId;
+      if (authService.hasOrganizationAdminPermission(currentCatalogId || roleCatalogId)) {
+        return true; // Found a matching role with admin permission
+      }
+    }
+    return false; // No matching role with admin permission
+  };
 
   return (
     <HeaderBase
@@ -22,6 +39,8 @@ const Header = () => {
       useDemoLogo={env.USE_DEMO_LOGO}
       homeUrl={FDK_REGISTRATION_BASE_URI}
       skeHomeText='Datakataloger'
+      showManageConceptCatalogsUrl={showManageConceptCatalogsUrl()}
+      manageConceptCatalogsUrl={CATALOG_ADMIN_BASE_URI}
     >
       <Link aria-label={localization.registerData[1]} href={`${FDK_BASE_URI}/guidance`}>
         {localization.registerData[0]}
