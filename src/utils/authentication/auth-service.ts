c6c67a988fb61aa10e1e76b25382a32ee2318372
Jeff Reiffers <jeffreiffers@gmail.com>
2025-03-21 09:41:13 +0100

    fix: app is available on path /catalogs not / (root)

--- src/utils/authentication/auth-service.ts
+++ src/utils/authentication/auth-service.ts
@@ -1,15 +1,15 @@
 import { Auth } from '.';
 import env from '../../utils/constants/env';
 
-const { SSO_HOST } = env;
+const { FDK_REGISTRATION_BASE_URI, SSO_HOST } = env;
 const OIDC_CLIENT_ID = 'fdk-registration-public';
 
 export const authService = new Auth({
   oidcIssuer: `${SSO_HOST}/auth/realms/fdk`,
   clientId: OIDC_CLIENT_ID,
   redirectUri: location.href,
-  logoutRedirectUri: location.origin,
-  silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
+  logoutRedirectUri: location.href,
+  silentCheckSsoRedirectUri: `${FDK_REGISTRATION_BASE_URI}/catalogs/silent-check-sso.html`,
 });
 
 export default authService;
